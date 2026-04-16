0b72cd64927636aa53a246ddbfc1d4c242ad4236
RJZInfoneo <162684001+RJZInfoneo@users.noreply.github.com>
2026-04-16 15:44:40 +0200

    Update printer installation script with corrections
    Added versioning to printer name and included official driver injection into Windows spooler.

--- install_hp.bat
+++ install_hp.bat
@@ -16,7 +16,9 @@ set "FOLDER_ROOT=C:\Admin\Imprimantes\Drivers\HP\new_drivers"
 set "ZIP_FILE=%FOLDER_ROOT%\drivers.zip"
 set "Pilote=%FOLDER_ROOT%\HPOneDriver.4081_V3_x64.inf"
 set "NomLocal=REFFYE COULEUR"
-set "Imprimante=HP Smart Universal Printing"
+
+:: CORRECTION 1 : Ajout du (V3) obligatoire
+set "Imprimante=HP Smart Universal Printing (V3)"
 
 echo [+] Dossiers...
 if not exist "%FOLDER_ROOT%" mkdir "%FOLDER_ROOT%" >nul 2>&1
@@ -35,13 +37,17 @@ if exist "%ZIP_FILE%" del /q "%ZIP_FILE%" >nul 2>&1
 echo [+] Port...
 powershell -Command "if (-not (Get-PrinterPort -Name \"IP_%IP%\" -ErrorAction SilentlyContinue)) { Add-PrinterPort -Name \"IP_%IP%\" -PrinterHostAddress \"%IP%\" }" >nul 2>&1
 
-echo [+] Pilote...
+echo [+] Pilote (Staging)...
 if exist "%Pilote%" (
     pnputil /add-driver "%Pilote%" /install >nul 2>&1
 ) else (
     echo [!] ERREUR : Pilote introuvable.
     exit /b
 )
+
+:: CORRECTION 2 : Injection officielle du pilote dans le spooler Windows
+echo [+] Pilote (Injection Spooler)...
+powershell -Command "Add-PrinterDriver -Name '%Imprimante%' -InfPath '%Pilote%' -ErrorAction SilentlyContinue" >nul 2>&1
 
 echo [+] Imprimante...
 powershell -Command "if (-not (Get-Printer -Name '%NomLocal%' -ErrorAction SilentlyContinue)) { Add-Printer -Name '%NomLocal%' -DriverName '%Imprimante%' -PortName \"IP_%IP%\" }" >nul 2>&1
