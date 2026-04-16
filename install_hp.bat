0a86a623439dbc8cb023c077cab4adbe76987145
RJZInfoneo <162684001+RJZInfoneo@users.noreply.github.com>
2026-04-16 15:31:55 +0200

    Suppress console output for various commands
    Redirect output of several commands to avoid cluttering the console and suppress error messages.

--- install_hp.bat
+++ install_hp.bat
@@ -1,4 +1,6 @@
 @echo off
+:: Force la console en UTF-8 pour eviter les plantages d'ESET
+chcp 65001 >nul
 SETLOCAL
 
 :: Verification du parametre IP
@@ -17,39 +19,37 @@ set "NomLocal=REFFYE COULEUR"
 set "Imprimante=HP Smart Universal Printing"
 
 echo [+] Dossiers...
-if not exist "%FOLDER_ROOT%" mkdir "%FOLDER_ROOT%"
+if not exist "%FOLDER_ROOT%" mkdir "%FOLDER_ROOT%" >nul 2>&1
 
 echo [+] Verification...
 if exist "%Pilote%" goto :SKIP_DOWNLOAD
 
 echo [+] Telechargement...
-powershell -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%URL_ZIP%' -OutFile '%ZIP_FILE%' -UseBasicParsing"
+powershell -Command "$ProgressPreference='SilentlyContinue'; [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri '%URL_ZIP%' -OutFile '%ZIP_FILE%' -UseBasicParsing" >nul 2>&1
 
 echo [+] Extraction...
-powershell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%FOLDER_ROOT%' -Force"
-if exist "%ZIP_FILE%" del /q "%ZIP_FILE%"
+powershell -Command "Expand-Archive -Path '%ZIP_FILE%' -DestinationPath '%FOLDER_ROOT%' -Force" >nul 2>&1
+if exist "%ZIP_FILE%" del /q "%ZIP_FILE%" >nul 2>&1
 
 :SKIP_DOWNLOAD
 echo [+] Port...
-powershell -Command "if (-not (Get-PrinterPort -Name \"IP_%IP%\" -ErrorAction SilentlyContinue)) { Add-PrinterPort -Name \"IP_%IP%\" -PrinterHostAddress \"%IP%\" }"
+powershell -Command "if (-not (Get-PrinterPort -Name \"IP_%IP%\" -ErrorAction SilentlyContinue)) { Add-PrinterPort -Name \"IP_%IP%\" -PrinterHostAddress \"%IP%\" }" >nul 2>&1
 
 echo [+] Pilote...
 if exist "%Pilote%" (
-    pnputil /add-driver "%Pilote%" /install
+    pnputil /add-driver "%Pilote%" /install >nul 2>&1
 ) else (
     echo [!] ERREUR : Pilote introuvable.
     exit /b
 )
 
 echo [+] Imprimante...
-powershell -Command "if (-not (Get-Printer -Name '%NomLocal%' -ErrorAction SilentlyContinue)) { Add-Printer -Name '%NomLocal%' -DriverName '%Imprimante%' -PortName \"IP_%IP%\" }"
+powershell -Command "if (-not (Get-Printer -Name '%NomLocal%' -ErrorAction SilentlyContinue)) { Add-Printer -Name '%NomLocal%' -DriverName '%Imprimante%' -PortName \"IP_%IP%\" }" >nul 2>&1
 
-:: Petite pause d'une seconde pour que le service d'impression digere l'ajout
 timeout /t 1 /nobreak >nul
 
 echo [+] Mise par defaut...
-:: Le parametre /q est CRUCIAL ici pour ne pas bloquer l'agent ESET (mode silencieux)
-rundll32 printui.dll,PrintUIEntry /y /n "%NomLocal%" /q
+rundll32 printui.dll,PrintUIEntry /y /n "%NomLocal%" /q >nul 2>&1
 
 echo [OK] Termine.
 exit /b
